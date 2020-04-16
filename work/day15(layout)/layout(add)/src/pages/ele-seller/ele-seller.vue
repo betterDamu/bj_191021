8ed8869de11b878e10b3389a6535ca60cca62ba0
damu <wjl@126.com>
2020-04-16 10:17:27 +0800

    0416补课

---
+++ work/day15(layout)/layout(add)/src/pages/ele-seller/ele-seller.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>seller</div>
+</template>
+
+<script>
+    import {mapActions} from "vuex";
+    import {GETSELLER} from "store/mutation_types"
+    export default {
+        name: "ele-seller",
+        methods:{
+            ...mapActions([GETSELLER])
+        },
+        mounted(){
+            this[GETSELLER]()
+        },
+        created(){
+            console.log("created ele-seller.vue")
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
